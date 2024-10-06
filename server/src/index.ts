73d1aa67f150ff2d036e771c3ac39e6ab64c2c8f
Baasir-stack <baasir121121@gmail.com>
2024-10-07 01:25:53 +0500

    updated backend issue

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,16 @@
-import app from './app';
 import { connectDB } from './config/db';
 import { setupGracefulShutdown } from './utils/gracefulShutdown';
 import { loadEnvironment } from './utils/env';
 import dotEnv from 'dotenv';
+import express, { Application } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser"; 
+import authRoutes from "./routes/auth.route";
+import appRoutes from "./routes/app.route";
+import subscriptionRoutes from "./routes/subscription.route";
+import userRoutes from "./routes/user.route";
+import subscriptionPlan from "./routes/subscription.plan.route";
+import errorMiddleware from "./middlewares/error"; 
 
 dotEnv.config({
     path: '.env',
@@ -14,6 +22,34 @@ loadEnvironment();
 // Define the port from environment variables
 const PORT = process.env.PORT || 8000;
 
+
+
+const app: Application = express();
+
+const corsOptions = {
+    origin: process.env.CLIENT_URL, 
+    credentials: true, 
+  }
+
+// Middleware
+app.use(cors(corsOptions));
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true })); 
+app.use(cookieParser());
+
+// API Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/app", appRoutes);
+app.use('/api/subs', subscriptionRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/subs-plan', subscriptionPlan);
+
+// Error handling middleware
+app.use(errorMiddleware);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the API");
+});
 
 // Start the server with a DB connection
 const startServer = async () => {
