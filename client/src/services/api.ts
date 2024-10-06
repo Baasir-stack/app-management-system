be7d3a08ead371f476f806bf8610a1e5be63c137
Baasir-stack <baasir121121@gmail.com>
2024-10-07 00:58:00 +0500

    fixed route issue

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const appApis = createApi({
     reducerPath: 'appApis',
     baseQuery: fetchBaseQuery({
-        baseUrl: `${process.env.REACT_APP_API_URL}/api/`,
+        baseUrl: `${process.env.REACT_APP_API_URL}`,
         prepareHeaders,
         credentials: 'include',
     }),
